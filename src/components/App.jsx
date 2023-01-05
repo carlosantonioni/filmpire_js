371454c86d082b49611508b3bce5b6b91d4bb555
carant06 <carant06@gmail.com>
2023-01-04 21:17:29 -0500

    added routes and navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router';
+import { Actors, MovieInformation, Movies, Profile, NavBar } from './';
+
+
+// Create /
+// create /detailsId
 
 const App = () =>  (
-  <div>App</div>
+  <div>
+    <CssBaseline />
+    <NavBar/>
+    <main>
+    <Routes>
+      <Route path="/movie/:id" element={<MovieInformation />} />
+        <Route path="/actors/:id" element={<Actors />} />
+        <Route path="/" element={<Movies />} />
+        <Route path="/profile/:id" element={<Profile />} />
+    </Routes>
+    </main>
+  </div>
 );
 
 export default App;
